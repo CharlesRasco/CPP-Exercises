f746f067abcb7d4140ecf732fb1d643054b57cc6
CharlesRasco <146395878+CharlesRasco@users.noreply.github.com>
2023-09-29 08:31:06 +0800

    Add files via upload

---
+++ Sample 6.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	double Salary,Days;
+	
+	cout<<"Enter Salary per Days:";
+	cin>>Salary;
+	
+	cout<<"Enter number of Days Work:";
+	cin>>Days;
+
+	cout<<"Your total Salary is:"<<Salary * Days;
+	
+	
+	
+	
+	
+	
+	return 0;
+}
