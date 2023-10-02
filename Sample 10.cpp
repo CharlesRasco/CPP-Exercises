5a19f85ef470a7615a1ae42023bfedcad6c1fead
CharlesRasco <146395878+CharlesRasco@users.noreply.github.com>
2023-10-02 13:49:42 +0800

    Add files via upload

---
+++ Sample 10.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	
+	int x;
+	
+	cout<<"Enter your age: ";
+	cin>> x;
+	
+	if(x >= 18){
+		
+		cout<<"You are in a legal age\n";
+		
+	}else{
+		
+		
+		cout<<"You are a minor";
+		
+	}
+	
+	
+	return 0;
+}
