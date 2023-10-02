0f6f859c4359eafa83fb468edce51168aa03a828
CharlesRasco <146395878+CharlesRasco@users.noreply.github.com>
2023-10-02 14:18:58 +0800

    Add files via upload

---
+++ Sample 12.cpp
@@ -0,0 +1,25 @@
+
+#include<iostream>
+using namespace std;
+
+int main (){
+	
+	
+	int x;
+	cout <<"Enter a number:";
+	cin>>x;
+	
+	if(x % 2 == 0){
+		
+		cout<<"Even Number";
+	}else{
+		
+		cout<<"Odd Number";
+	}
+	
+	
+	
+	
+	
+	return 0;
+}
