24669a5eec5f29a7aa1baec4225c17498cc639da
CharlesRasco <146395878+CharlesRasco@users.noreply.github.com>
2023-10-02 14:02:30 +0800

    Add files via upload

---
+++ Sample 11.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	
+	int grade;
+	
+	cout<<"Enter your Grade: ";
+	cin>>grade;
+	
+	if(grade >= 75){
+		
+		cout<<"Passed\n";
+		
+	}else{
+		
+		
+		cout<<"Failed";
+		
+	}
+	
+	
+	return 0;
+}
